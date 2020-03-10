6d08c3082ea35cd1ba0f68cdb15b47a17f4656aa
StellaPOS <60994967+StellaPOS@users.noreply.github.com>
2020-03-10 00:38:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
-//var ParseDashboard = require('parse-dashboard');
 var path = require('path');
-
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -17,23 +15,11 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'stellaposid',
   masterKey: process.env.MASTER_KEY || 'stellaposmaster', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://stellapos.herokuapp.com/parse',  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"]
   }
 });
-
-// var dashboard = new ParseDashboard({
-//   "allowInsecureHTTP": true,
-//   "apps": [
-//     {
-//       "serverURL": "https://stellapos.herokuapp.com/parse",
-//       "appId": "stellaposid",
-//       "masterKey": "stellaposmaster",
-//       "appName": "stellapos"
-//     }
-//   ]
-// });
 
 var app = express();
 
@@ -43,8 +29,6 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
-
-//app.use('/dashboard', dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
