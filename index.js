dfafb192fc1560ded2927b61219aba673798aeed
StellaPOS <60994967+StellaPOS@users.noreply.github.com>
2020-03-10 00:58:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
@@ -17,9 +18,12 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || 'stellaposmaster', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"]
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
 
 var app = express();
 
